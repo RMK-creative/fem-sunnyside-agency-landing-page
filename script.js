2ce9a9661d06ca7ebe958a376e965e18a066d6b5
rmk <rachaelmkunz@gmail.com>
2021-07-28 14:00:34 +0200

    add modal toggle hidden to the click event handler

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".navigation__toggle");
+const modal = document.querySelector(".hidden");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
+  modal.classList.toggle("hidden");
 });
