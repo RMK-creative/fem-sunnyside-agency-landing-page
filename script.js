3b2daf734321c328e75694e85a4a3fc18abafd10
rmk <rachaelmkunz@gmail.com>
2021-08-02 16:59:09 +0200

    animate burger menu

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const hamburger = document.querySelector(".navigation__toggle");
-const modal = document.querySelector(".hidden");
+const hamburger = document.querySelector(".toggle-box");
+const modal = document.querySelector(".navbar__modal");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
-  modal.classList.toggle("hidden");
+  modal.classList.toggle("active");
 });
+
+// function showMenu() {
+//   document.querySelector(".navbar__modal").classList.toggle("active");
+//   document.querySelector(".menu").classList.toggle("hide");
+//   document.querySelector(".close").classList.toggle("show");
+// }
