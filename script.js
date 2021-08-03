15b3043fd2d0299cace23fcb20c30316aaa42bc0
rmk <rachaelmkunz@gmail.com>
2021-08-03 13:03:08 +0200

    remove unused code

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   modal.classList.toggle("active");
 });
-
-// function showMenu() {
-//   document.querySelector(".navbar__modal").classList.toggle("active");
-//   document.querySelector(".menu").classList.toggle("hide");
-//   document.querySelector(".close").classList.toggle("show");
-// }
